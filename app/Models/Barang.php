8b42b6cb6ba2d2475678f92870dc0db575301ff5
dhann666 <ramadhanharinugroho@gmail.com>
2025-03-21 20:20:48 +0700

    menyelesaikan modul master data barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Barang extends Model
+{
+
+    protected $table = 'barang'; // Nama tabel eksplisit
+
+    protected $guarded = [];
+
+    public static function getKodeBarang()
+    {
+        // query kode perusahaan
+        $sql = "SELECT IFNULL(MAX(Kode_barang), 'AB000') as Kode_barang 
+                FROM barang ";
+        $kodebarang = DB::select($sql);
+
+        // cacah hasilnya
+        foreach ($kodebarang as $kdbrg) {
+            $kd = $kdbrg->Kode_barang;
+        }
+        // Mengambil substring tiga digit akhir dari string PR-000
+        $noawal = substr($kd,-3);
+        $noakhir = $noawal+1; //menambahkan 1, hasilnya adalah integer cth 1
+        $noakhir = 'AB'.str_pad($noakhir,3,"0",STR_PAD_LEFT); //menyambung dengan string PR-001
+        return $noakhir;
+
+    }
+
+    // Dengan mutator ini, setiap kali data harga_barang dikirim ke database, koma akan otomatis dihapus.
+    public function setHargaBarangAttribute($value)
+    {
+        // Hapus koma (,) dari nilai sebelum menyimpannya ke database
+        $this->attributes['harga_barang'] = str_replace('.', '', $value);
+    }
+}
+
